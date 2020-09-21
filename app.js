fc5d40913f5c7f8591aed56e2647f08e03396c58
clam169 <jojilam@gmail.com>
2020-09-21 16:27:45 -0700

    final

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var temperature = 104;
-var newtemperature = (temperature - 32) / 1.8;
-console.log(temperature + " fahrenheit converts to " + newtemperature + " celsius.");
-if (newtemperature <= 10){
-    console.log("It is cold");
-}else if (newtemperature <= 20){
-    console.log("It is warm");
-}else if (newtemperature <= 100){
-    console.log("It is very hot");
+
+function addtemperature(temperature){
+    newtemperature = (temperature - 32) / 1.8;
+    console.log(temperature + " Fahrenheit converts to " + newtemperature + " Celcius.");
+    if (newtemperature <= 5){
+        console.log("It is very cold");
+    }else if (newtemperature <= 15){
+        console.log("It is cold");
+    }else if (newtemperature <= 25){
+        console.log("It is warm");
+    }else if (newtemperature <= 100){
+        console.log("It is very hot")};
 }
+    
+addtemperature(15);
+addtemperature(100);
--- favmovies.js
+++ favmovies.js
@@ -1,9 +1,7 @@
 var favouriteMovies = []
 
 function addFavouriteMovie(movieName){
-    if(movieName.includes("The")){;
-    return(favouriteMovies);
-    } else {
+    if(!movieName.includes("The")){;    
         favouriteMovies.push(movieName);
     }
 }
@@ -12,6 +10,13 @@ addFavouriteMovie("Spiderman");
 addFavouriteMovie("The awakening");
 addFavouriteMovie("The movie");
 addFavouriteMovie("SHAOLIN SOCCER")
+addFavouriteMovie("Jurassic Park")
+addFavouriteMovie("God of Gamblers")
 
+console.log("Favourite movies: " + favouriteMovies.length);
 
-console.log("My favourite movies: " + favouriteMovies)
+var loop = 0
+while (loop < favouriteMovies.length){
+    console.log(favouriteMovies[loop]);
+    loop ++;
+}
