b7c7119d7871a084abcc1c6fca21b7eb26fe4177
clam169 <jojilam@gmail.com>
2020-09-26 23:43:49 -0700

    working calendar

--- test.js
+++ test.js
@@ -3,10 +3,7 @@ function isLeapYear(year) {
 }
  
 function getDayOfTheWeek(year, month, day) {
-    var dayOfTheWeek = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];        
-
     var monthcode;
-
     if (month == 4 || month == 7){
         monthcode = 0;
     } else if (month == 1 || month == 10){
@@ -39,48 +36,22 @@ function getDayOfTheWeek(year, month, day) {
     }
  
     //calculating the day of the week//
-
-    // var yy = (String(year).charAt((String(year).length)-2)) + 
-    // (String(year).charAt((String(year).length)-1))
-
-    var dayOfTheWeekCode = ((~~((year%100)/12) + ((year%100)%12) + (~~(((year%100)%12) / 4)) + day + monthcode) % 7);
+    var dayOfTheWeekCode = ((Math.floor(year % 100 / 12) + ((year % 100) % 12) + Math.floor((year % 100 % 12) / 4) + day + monthcode) % 7);
+    var dayOfTheWeek = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
     var dayofWeekValue = dayOfTheWeek[dayOfTheWeekCode];
     return dayofWeekValue;
-
-    // var dayOfTheWeek = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    // var nMonths = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12];
-    // var nDays = 1
-    // console.log(dayOfTheWeekCode);
-    // console.log(dayOfTheWeekCode);
-
-
-
-    // for (var i = 0; i < 30; i++){
-    //     getDayOfTheWeek(2020, 01, i);
-    //     console.log(nMonths[0]+"-"+(nDays+i) + "-" + year +" is a " + dayOfTheWeek[dayOfTheWeekCode]);
-    //  }
 }
 
 function makeCalendar() {
-    var year = 2019;
-    var month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var year = 2020;
+    var month_days = [1, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     //var month_names = ["01", "02", "03"];
-    for (var i = 0; i < 12; i++) {
+    for (var i = 1; i < 13; i++) {
         for (var j = 1; j <= month_days[i]; j++) {
             var dayOfWeek = getDayOfTheWeek(year, i, j);
-            console.log(i + "-" + j + "-" + dayOfTheWeekCode);
+            console.log(i + "-" + j + "-" + year + " is a " + dayOfWeek);
         }
     }
-
-makeCalendar();
-    
-
 }
-
 makeCalendar();
-
-
-//getDayOfTheWeek(1989, 08, 16);
-
-//getDayOfTheWeek(2020, 01, 02);
 
