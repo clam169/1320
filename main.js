41bf27f3551b3f12e89cbb4e22f3cac821601766
clam169 <jojilam@gmail.com>
2020-11-11 19:22:41 -0800

    milestone 1 complete

---
+++ main.js
@@ -0,0 +1,22 @@
+/*
+ * Project: COMP1320 Milestone 1
+ * File Name: main.js
+ * Description: 
+ * 
+ * Created Date: November 11, 2020
+ * Author: Jo Jo Lam
+ * 
+ */
+
+const { unzip } = require("unzipper");
+
+const IOhandler = require("./IOhandler"),
+  zipFilePath = `${__dirname}/myfile.zip`,
+  pathUnzipped = `${__dirname}/unzipped`,
+  pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+.then((pathOut) => IOhandler.readDir(pathOut))
+.then((pngList) => IOhandler.grayScale(pngList, pathProcessed))
+.then((done) => console.log(done))
+.catch((msg) => console.log(msg))
